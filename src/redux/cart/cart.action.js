dc044bade07e4c54d1b60d80558bb93a6333b53b
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2021-01-08 07:30:07 +0530

    Added decrease and increase quantity of cart items in checkout page

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,6 +1,6 @@
 import CartActionTypes from "./cart.types";
 
-const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
+const { TOGGLE_CART_HIDDEN, ADD_ITEM, CLEAR_ITEM_FROM_CART, REMOVE_ITEM } = CartActionTypes;
 
 export const toggleCartHidden = () => {
   return {
@@ -11,6 +11,20 @@ export const toggleCartHidden = () => {
 export const addItem = (item) => {
   return {
     type: ADD_ITEM,
+    payload: item
+  };
+};
+
+export const clearItemFromCart = (item) => {
+  return {
+    type: CLEAR_ITEM_FROM_CART,
+    payload: item
+  };
+};
+
+export const removeItem = (item) => {
+  return {
+    type: REMOVE_ITEM,
     payload: item
   };
 };
