6d7f538af842bf16eb8c21bf12d7748373abdbc6
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2021-01-11 16:05:17 +0530

    Added Nested Routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import CollectionOverview from "../../components/collections-overview/collections-overview.component";
+import { Route } from "react-router-dom";
 
-const ShopPage = () => {
+import CollectionOverview from "../../components/collections-overview/collections-overview.component";
+import CategoryPage from "../category/category.component";
+
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
     </div>
   );
 };
